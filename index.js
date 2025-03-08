0ecc33c3e10c412158cce13a893b3780e6f09114
Filipi Martins <90717547+FipaLipe@users.noreply.github.com>
2025-03-08 14:10:35 +0000

    Setup inicial do socket.io

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createServer } from "node:http";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server)
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const indexPath = join(__dirname, 'index.html')
+
+app.get('/',(req, res) => {
+    res.sendFile(indexPath)
+})
+
+io.on('connection', (socket) => {
+    console.log('O usuário logou! ' + socket.id)
+})
+
+server.listen(3000, () => {
+    console.log('O servidor tá rodando na porta 3000!')
+})
